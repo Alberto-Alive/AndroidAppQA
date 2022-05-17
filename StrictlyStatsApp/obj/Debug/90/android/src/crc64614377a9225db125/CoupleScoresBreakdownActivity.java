02beb5e9629bb333cf4e672bb2420f3a27c4682d
unknown <albertopopescu@outlook.com>
2022-05-17 13:15:18 +0100

    first commit

---
+++ StrictlyStatsApp/obj/Debug/90/android/src/crc64614377a9225db125/CoupleScoresBreakdownActivity.java
@@ -0,0 +1,47 @@
+package crc64614377a9225db125;
+
+
+public class CoupleScoresBreakdownActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("StrictlyStats.CoupleScoresBreakdownActivity, StrictlyStats", CoupleScoresBreakdownActivity.class, __md_methods);
+	}
+
+
+	public CoupleScoresBreakdownActivity ()
+	{
+		super ();
+		if (getClass () == CoupleScoresBreakdownActivity.class)
+			mono.android.TypeManager.Activate ("StrictlyStats.CoupleScoresBreakdownActivity, StrictlyStats", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
